8d23fbc7fffd8b30bd735885269b6d4cc3a09ade
Jaeyeol Lee <rijgndqw012@gmail.com>
2022-04-19 10:44:36 +0900

    [Django TDD] Complete Chapter 2

--- python/django-tdd/functional_test.py
+++ python/django-tdd/functional_test.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert 'success' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
