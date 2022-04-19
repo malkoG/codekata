dce7bea9aee1322db441169eb4c3c4571b3264e7
Jaeyeol Lee <rijgndqw012@gmail.com>
2022-04-19 11:01:14 +0900

    [Django TDD] Complete Chapter 3

--- python/django-tdd/superlists/superlists/urls.py
+++ python/django-tdd/superlists/superlists/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from lists.views import home_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page, name='home')
 ]
